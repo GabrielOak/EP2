709ed4127f6be1abecb030f0af1325815ede2dae
Gabriel Carvalho <gabriel315df@gmail.com>
2017-11-05 16:18:59 -0200

    Criação da classe AnguloFaseHarmonico

---
+++ src/model/AnguloFaseHarmonico.java
@@ -0,0 +1,17 @@
+package model;
+
+public class AnguloFaseHarmonico {
+	int anguloFaseHarmonico;
+	
+	public AnguloFaseHarmonico() {
+		
+	}
+	
+	public int getAnguloFaseHarmonico() {
+		return anguloFaseHarmonico;
+	}
+	
+	public void setAnguloFaseHarmonico(int anguloFaseHarmonico) {
+		this.anguloFaseHarmonico = anguloFaseHarmonico;
+	}
+}

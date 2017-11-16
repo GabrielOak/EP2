e4912867769a2f6f7f2b5218bfe7bb8608076445
Gabriel Carvalho <gabriel315df@gmail.com>
2017-11-16 02:04:28 -0200

    Criação da main

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.io.IOException;
+import view.MenuInicial;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		
+		MenuInicial menu = new MenuInicial();
+
+	}
+
+}
